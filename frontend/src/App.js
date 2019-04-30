79f148121043d9168722ec4c277c6f34d8f07349
Rohitwable <rohitwable46@gmail.com>
2019-04-30 23:55:20 +0530

    client routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch , Redirect} from 'react-router-dom'
+import AuthPage from './pages/auth'
+import EventPage from './pages/events'
+import BookingPage from './pages/bookings'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello </h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Redirect from='/' to='/auth' exact/>
+        <Route path='/auth' component={AuthPage} />
+        <Route path='/events' component={EventPage} />
+        <Route path='/bookings' component={BookingPage} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
